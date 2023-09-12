e501de8e5810aa4b3c646ec1d4d01bbf8e44a539
Srafi3107 <srafi3107@gmail.com>
2023-09-12 16:28:15 +0600

    added

--- 3np_backend/src/Admin/Admin.controller.ts
+++ 3np_backend/src/Admin/Admin.controller.ts
@@ -3,7 +3,7 @@ import { AdminService } from "./Admin.service";
 import { AdminDTO } from "./Admin.dto";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { MulterError, diskStorage } from "multer";
-import { AdminEntity } from "./Admin.entity";
+import { AdminEntity, OTPEntity } from "./Admin.entity";
 import * as bcrypt from 'bcrypt';
 import { SessionGuard } from "./session.guard";
 import { ManagerDto } from "src/Manager/manager.dto";
@@ -19,13 +19,19 @@ import { PoliceService } from "src/Police/police.service";
 export class AdminController{
     constructor(private readonly adminservice:AdminService){}
     
+    @Get('/index')
+    //@UseGuards(SessionGuard)
+    //@UsePipes(new ValidationPipe())
+    getIndex(): any {
+        return "Admin is Good";
+    }
 //1. pass:Abc123
 //2.pass:Abcd12345
 @Post("/addadmin")
 @UsePipes(new ValidationPipe())
 async addadmin(@Body() data: AdminDTO): Promise<string> {
-  const salt = await bcrypt.genSalt();
-  data.password = await bcrypt.hash(data.password, salt);
+  // const salt = await bcrypt.genSalt();
+  // data.password = await bcrypt.hash(data.password, salt);
 
   const result = await this.adminservice.create(data);
   if (result) {
@@ -42,7 +48,9 @@ async addadmin(@Body() data: AdminDTO): Promise<string> {
   async sendemailtopolice(@Param('username') username: string): Promise<PRegistrationEntity> {
     return this.adminservice.sendEmailToPolice(username);
   }
+  
 @Post("/addmanager")
+@UseGuards(SessionGuard)
 @UsePipes(new ValidationPipe())
 async addmanager(@Body() data: ManagerDto): Promise<string> {
   const salt = await bcrypt.genSalt();
@@ -55,73 +63,170 @@ async addmanager(@Body() data: ManagerDto): Promise<string> {
     return "Account already exists";
   }
 }
-@Post('/addpolice')
-// @UseGuards(SessionGuard)
-  @UsePipes(new ValidationPipe())
-  async addPolice(@Session() session, @Body() police: PRegistrationEntity): Promise<PRegistrationEntity> {
-    const admin = await this.adminservice.getAdminById(session.adminId); // Assuming you have the AdminId stored in the session as "adminId"
-    police.admins = [admin];
-
-    return  this.adminservice.addPolice(police);
-   
-  }
-  @Post('/addVictim')
-// @UseGuards(SessionGuard)
-  @UsePipes(new ValidationPipe())
-  async addVictim(@Session() session, @Body() victim: VictimEntity): Promise<VictimEntity> {
-    const admin = await this.adminservice.getAdminById(session.adminId); // Assuming you have the AdminId stored in the session as "adminId"
-    victim.admins = [admin];
-
-    return this.adminservice.AddVictim(victim);
-  }
 // @Post('/addpolice')
 // // @UseGuards(SessionGuard)
-//   @UsePipes(new ValidationPipe())
-//   async addPolice(@Session() session, @Body() police: PoliceEntity): Promise<PoliceEntity> {
+//   // @UsePipes(new ValidationPipe())
+//   async addPolice(@Session() session, @Body() police: PRegistrationEntity): Promise<PRegistrationEntity> {
 //     const admin = await this.adminservice.getAdminById(session.adminId); // Assuming you have the AdminId stored in the session as "adminId"
 //     police.admins = [admin];
 
 //     return  this.adminservice.addPolice(police);
    
 //   }
-//   @Post('/addVictim')
-// @UseGuards(SessionGuard)
-//   @UsePipes(new ValidationPipe())
-//   async addVictim(@Session() session, @Body() victim: VictimEntity): Promise<VictimEntity> {
-//     const admin = await this.adminservice.getAdminById(session.adminId); // Assuming you have the AdminId stored in the session as "adminId"
-//     victim.admins = [admin];
 
-//     return this.adminservice.AddVictim(victim);
+@Post('/addpolice/:adminid')
+@UsePipes(new ValidationPipe())
+
+    @UseInterceptors(FileInterceptor('mypicfile',
+
+    {
+
+        fileFilter: (req, file, cb) => {
+
+            if (file.originalname.match(/^.*\.(jpg|webp|png|jpeg)$/))
+
+                cb(null, true);
+
+            else {
+
+                cb(new MulterError('LIMIT_UNEXPECTED_FILE', 'image'), false);
+
+            }
+
+        },
+
+        limits: { fileSize: 39000000 }, // 39 MB
+
+        storage:diskStorage({
+
+            destination: './assets/profile_images',
+
+            filename: function (req, file, cb) {
+
+                cb(null,Date.now()+file.originalname)
+
+            },
+
+        })
+
+    }
+
+))
+  // @UseGuards(SessionGuard)
+  async addpolice(@Param("adminid") adminid:number,@UploadedFile() myfileobj: Express.Multer.File, @Body() police: PRegistrationEntity): Promise<{ message: string }> {
+    // const adminId = session.adminId; // Assuming you have the AdminId stored in the session as "adminId"
+    
+    // console.log('Retrieved Admin ID from session:', adminId);
+    // console.log('Session data:', session.id);
+
+    police.profile_image = myfileobj.filename;
+    const addedpolice = await this.adminservice.addPoliceWithAdmin( adminid,police);
+    
+    if (addedpolice) {
+      return { message: 'Police added successfully.' };
+    } else {
+      return { message: 'Failed to add Police.' };
+    }
+  }
+  // @Post('/addVictim')
+  // // @UseGuards(SessionGuard)
+  //   // @UsePipes(new ValidationPipe())
+  //   async addVictim(@Session() session, @Body() victim: VictimEntity): Promise<VictimEntity> {
+  //     const admin = await this.adminservice.getAdminById(session.adminId); // Assuming you have the AdminId stored in the session as "adminId"
+  //     victim.admins = [admin];
+  
+  //     return this.adminservice.AddVictim(victim);
+    // }
+  @Post('/addVictim/:adminid')
+  @UsePipes(new ValidationPipe())
+  @UseInterceptors(FileInterceptor('image',
+  {
+      fileFilter: (req, file, cb) => {
+          if (file.originalname.match(/^.*.(jpg|webp|png|jpeg)$/))
+              cb(null, true);
+          else {
+              cb(new MulterError('LIMIT_UNEXPECTED_FILE', 'image'), false);
+          }
+      },
+      limits: { fileSize: 6000000 },
+      storage: diskStorage({
+          destination: './uploads',
+          filename: function (req, file, cb) {
+              cb(null, Date.now() + file.originalname)
+          },
+      })
+  }
+))
+
+  // @UseGuards(SessionGuard)
+  async addVictim(@Param("adminid") adminid:number, @UploadedFile() imageobj:Express.Multer.File, @Body() victim: VictimEntity): Promise<{ message: string }> {
+    // const adminId = session.adminId; // Assuming you have the AdminId stored in the session as "adminId"
+    
+    // console.log('Retrieved Admin ID from session:', adminId);
+    // console.log('Session data:', session.id);
+
+    console.log(imageobj.filename);
+    victim.Insertfile_NID= imageobj.filename;
+    const addedVictim = await this.adminservice.addVictimWithAdmin( adminid,victim);
+    
+    if (addedVictim) {
+      return { message: 'Victim added successfully.' };
+    } else {
+      return { message: 'Failed to add victim.' };
+    }
+  }
+//   @Post('/addVictim')
+// // @UseGuards(SessionGuard)
+// async addVictim(
+//   @Session() session,
+//   @Body() victim: VictimEntity
+// ): Promise<{ message: string }> {
+//   const adminId = session.adminId; // Assuming you have the AdminId stored in the session as "adminId"
+  
+//   console.log('Retrieved Admin ID from session:', adminId);
+//   console.log('Session data:', session);
+
+//   if (adminId) {
+//     const admin = await this.adminservice.getAdminById(adminId);
+//     if (admin) {
+//       victim.admins = [admin];
+//       await this.adminservice.addVictimWithAdmin(adminId,victim);
+//       return { message: 'Victim added successfully.' };
+//     } else {
+//       return { message: 'Failed to add victim. Admin not found.' };
+//     }
+//   } else {
+//     return { message: 'Failed to add victim. Admin ID not found in session.' };
 //   }
-//updateadminbyid
+// }
+ 
+
 
 
 @Put('/updateadmin/:AdminId')
-@UseGuards(SessionGuard)
-@UsePipes(new ValidationPipe())
+// @UseGuards(SessionGuard)
+// @UsePipes(new ValidationPipe())
 updateAdminbyID(@Param('AdminId') AdminId: number, @Body() data: AdminDTO): Promise<AdminEntity> {
   return this.adminservice.updateAdminById(AdminId, data);
 }
 @Get("/adminprofile/:id")
-@UseGuards(SessionGuard)
+// @UseGuards(SessionGuard)
 async getAdminProfile(@Param('id',ParseIntPipe) id: number) {
   return this.adminservice.getAdminProfilebyid(id);
 }
-
-
-
-
-// @Post("/addmanager")
-// @UseGuards(SessionGuard)
-// @UsePipes(new ValidationPipe())
-// async addManager(@Session() session,@Body() manager):Promise<ManagerEntity> {
-//   return this.adminservice.addManager(manager);
+// postcom(@Body() data:AdminDTO):object{
+  
+//   return this.adminservice.getAdminProfilebyid(data);
 // }
+
+
+
+
 
 
 //getpolice using username
 @Get("/getpolice/:username")
-@UseGuards(SessionGuard)
+// @UseGuards(SessionGuard)
   async getPoliceInfoByUsername(@Param('username') username: string): Promise<PRegistrationEntity> {
     return this.adminservice.getPoliceInfoByUsername(username);
   }
@@ -129,34 +234,34 @@ async getAdminProfile(@Param('id',ParseIntPipe) id: number) {
 
   //delete police account using username 
   @Delete('/deletepolice/:username')
-  @UseGuards(SessionGuard)
-  @UsePipes(new ValidationPipe())
+  // @UseGuards(SessionGuard)
+  // @UsePipes(new ValidationPipe())
   async deletePolice(@Param('username') username: string): Promise<PRegistrationEntity> {
     return this.adminservice.deletePoliceAccount(username);
   }
   //update police account using username 
   @Put('/updatepolice/:username')
-  @UseGuards(SessionGuard)
+  // @UseGuards(SessionGuard)
   // @UsePipes(new ValidationPipe())
   updatePolice(@Param('username') username: string, @Body() data: PRegistrationDTO): Promise<PRegistrationEntity> {
     return this.adminservice.updatePoliceAccount(username, data);
   }
 
   @Get("/getVictim/:id")
-@UseGuards(SessionGuard)
+// @UseGuards(SessionGuard)
   async getVictimById(@Param('id') id: number): Promise<VictimEntity> {
     return this.adminservice.getVictimById(id);
   }
   @Put('/updatevictim/:id')
-  @UseGuards(SessionGuard)
-  @UsePipes(new ValidationPipe())
+  // @UseGuards(SessionGuard)
+  // @UsePipes(new ValidationPipe())
   updatevictimbyid(@Param('id') id: number, @Body() data: VicDTO): Promise<VictimEntity> {
     return this.adminservice.updatevictimbyid(id, data);
   }
 
   @Delete('/deletevictim/:id')
-  @UseGuards(SessionGuard)
-  @UsePipes(new ValidationPipe())
+  // @UseGuards(SessionGuard)
+  // @UsePipes(new ValidationPipe())
   async deletevictimbyid(@Param('id') id: number): Promise<VictimEntity> {
     return this.adminservice.deletevictimbyid(id);
   }
@@ -240,168 +345,63 @@ logout(@Session() session)
   }
 }
 
+@Post('/send-otp/:adminid')
+  async sendOTP(@Param('adminid') adminid: number): Promise<AdminEntity> {
+    try {
+      const admin = await this.adminservice.sendOTP(adminid);
+      return admin;
+    } catch (error) {
+      throw new NotFoundException(error.message);
+    }
+  }
 
+  @Post('/verify-otp/:adminid')
+  async verifyOTP(
+    @Param('adminid') adminid: number,
+    @Body('otp') otp: string,
+  ): Promise<string> {
+    try {
+      const result = await this.adminservice.verifyOTP(adminid, otp);
+      return result;
+    } catch (error) {
+      throw new NotFoundException(error.message);
+    }
+  }
+  // @Post('/verify-otp/:otp')
+  // async verifyOTP(
+  //   @Param('otp') otp: string,
+  //   @Body() adminid: number
+  // ): Promise<string> {
+  //   try {
+  //     const result = await this.adminservice.verifyOTP(otp,adminid);
+  //     return result;
+  //   } catch (error) {
+  //     throw new NotFoundException(error.message);
+  //   }
+  // }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // @Put('/update-password/:adminid')
+  // async updatePassword(
+  //   @Param('adminid') adminid: number,
+  //   @Body('newPassword') newPassword: AdminDTO,
+  // ): Promise<AdminEntity> {
+  //   try {
+  //     const admin = await this.adminservice.updateAdminById(adminid, newPassword);
+  //     return admin;
+  //   } catch (error) {
+  //     throw new NotFoundException(error.message);
+  //   }
+  // }
+  @Put('/update-password/:adminid')
+  async updatePassword(
+    @Param('adminid') adminid: number,
+    @Body('newPassword') newPassword: string,
+  ): Promise<AdminEntity> {
+    try {
+      const admin = await this.adminservice.updatePassword(adminid, newPassword);
+      return admin;
+    } catch (error) {
+      throw new NotFoundException(error.message);
+    }
+  }
 }
-
-
-    
-   
-//      @Put("/VictimUpdate/:id")
-//      @UsePipes(new ValidationPipe())
-//      updatevictimbyid(@Param('id') id:number, @Body() data:VicDTO):object{
-//         return this.adminservice.updateVictimbyid(id,data);
-//      }
-   
-//      @Delete("/VicDelete/:id")
-//      deleteadmin(@Param('id') id:number):string{
-//        return this.adminservice.DeleteVictimBYID(id);
-//      }
-   
-//      //   @Post("/addadmin/:id")
-//      //   @UsePipes(new ValidationPipe())
-//      //    addadminbyid(@Param() id:number, @Body() data:AdminDTO):object{
-//      //      debugger;
-//      //     return this.adminservice.save(id,data);
-//      // }
-//      @Get("/search/:id")
-//      getVictimbyId(@Param('id',ParseIntPipe) id:number):object{
-//          return this.adminservice.getVictimById(id);
-   
-//      }
-//      @Get("/Searchadmin/:id")
-//      getadminbyId(@Param('id',ParseIntPipe) id:number):object{
-//          return this.adminservice.getAdminbyid(id);
-   
-//      }
-   
-//    //   @Post("/addvictim/:id")
-//    //  @UsePipes(new ValidationPipe())
-//    //   addVictim( @Param() id:number, @Body() victim:VicDTO){
-//    //     debugger
-//    //     return this.adminservice.addVictim(id,victim);
-//    // }
-//    @Post('/addvictim/:id')
-//    @UsePipes(new ValidationPipe())
-//    async addVictim(@Param('id') id: number, @Body() victim: VicDTO): Promise<VictimEntity> {
-//      return this.adminservice.addVictim(id, victim);
-//    }
-   
-   
-   
-//        //Feature-1
-//        @Post("login")
-//        @UsePipes(new ValidationPipe())
-//        login(@Query() qry:loginDTO):object{
-//         return this.adminservice.login(qry);
-//         }
-   
-     
-//        //Feature-2
-//        @Get("profile")
-//        getAdminProfile(@Query() qry:AdminDTO):object{
-//            return this.adminservice.getAdminProfile(qry);
-           
-//        }
-       
-   
-//        //Feature-3
-//        @Put("/adminupdateProfile")
-//        updateadmin(@Query() data:AdminDTO):object{
-//           return this.adminservice.updateadmin(data);
-//        }
-   
-   
-      
-//        //Feature-4
-//        @Get("Policeprofile")
-//        getPoliceProfilebyName(@Param() username:string,  @Body() qry:RegistrationDTO):object{
-//            return this.adminservice.getPoliceProfile(qry);
-           
-//        }
-      
-   
-//        //Feature-5
-//            @Put("/policeupdate/:email")
-//            @UsePipes(new ValidationPipe())
-//            updatePolicebyemail(@Param('email') email: string, @Body() data: RegistrationDTO): object {
-//            return this.adminservice.updatePolicebyemail(email, data);
-//        }
-      
-      
-//        //Feature-6
-//        @Get("/Victimprofile")
-//        getVictimProfilebyName(@Query() qry:VicDTO):object{
-//            return this.adminservice.getVictimProfilebyName(qry);
-           
-//        }
-       
-//        //Feature-7
-//        @Put("/victimupdate/:VicID")
-//        @UsePipes(new ValidationPipe())
-//        updateVictimbyid(@Param() VicID: number, @Body() data: VicDTO): object {
-//        return this.adminservice.updateVictimbyid(VicID, data);
-//    }
-       
-   
-//        //Feature-8
-//        @Delete('/Victimdelete/:VicID')
-//        @UsePipes(new ValidationPipe())
-//        deleteVictimbyid(@Param() VicID: number, data:VicDTO): object {
-//        return this.adminservice.deleteVictimbyid(VicID,data);
-//        }
-   
-//        @Post(('/uploadProfile'))
-//        @UseInterceptors(FileInterceptor('myfile',
-//          {
-//            fileFilter: (req, file, cb) => {
-//              if (file.originalname.match(/^.*\.(jpg|webp|png|jpeg|jfif|docx)$/)) {
-//                cb(null, true);
-//              } else {
-//                cb(new MulterError('LIMIT_UNEXPECTED_FILE', 'image'), false);
-//              }
-//            },
-//            limits: { fileSize: 30000 },
-//            storage: diskStorage({
-//              destination: './uploads',
-//              filename: function (req, file, cb) {
-//                cb(null, Date.now() + file.originalname);
-//              },
-//            }),
-//          }
-//        ))
-//        uploadFile(@UploadedFile() myfileprof: Express.Multer.File): object {
-//          if (myfileprof) {
-         
-//            console.log(myfileprof);
-        
-       
-//            return { message: "File uploaded successfully " };
-//          } else {
-//            return { message: "No file uploaded" };
-//          }
-//        }
-       
-   
-
-
-// }
-
-// // @Delete('/policedelete')
-// //     @UsePipes(new ValidationPipe())
-// //     deletePoliceByEmail(@Body() data:RegistrationDTO): object {
-// //     return this.adminservice.deletePoliceByEmail(data);
-// //     }
